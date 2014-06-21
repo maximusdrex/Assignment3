39582565229d5fe1302952f51df52e6a7696c07b
S <s>
2014-06-20 21:31:30 -0700

    {"type":"TI", "runs":"0"}

--- Classes.java
+++ Classes.java
@@ -10,6 +10,11 @@ public class Classes {
 public int loop = 1;
 	public void run() {
 		GRect rect = new GRect(100, 100, 100, 100);
+		add(rect);
+		while(loop < 0) {
+			rect.move(3, 2);
+			add(rect);
+		}
 		
 	}
 }
