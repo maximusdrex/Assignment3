3451cac5dbec6bce82dfb80033f76089648e4176
S <s>
2014-06-20 21:29:45 -0700

    {"type":"TI", "runs":"0"}

--- Classes.java
+++ Classes.java
@@ -1,4 +1,15 @@
 
-public class Classes {
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class Classes {
+public var loop = 1;
+	public void run() {
+		GRect rect = new GRect(100, 100, 100, 100);
+		
+	}
 }
