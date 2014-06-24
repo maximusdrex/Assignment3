c819d34846c3243967621bc87ed970c2e4ef452f
S <s>
2014-06-23 21:41:40 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -61,6 +61,7 @@ public class Breakout extends GraphicsProgram {
 		addMouseListeners();
 		makeBricks();
 		GRect paddle = new GRect(WIDTH / 2, HEIGHT - PADDLE_Y_OFFSET, PADDLE_WIDTH, PADDLE_HEIGHT);
+		paddle.setFilled(true);
 		add(paddle);
 		
 	}
