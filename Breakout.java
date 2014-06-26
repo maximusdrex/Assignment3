6333ed0ab72359a85a5b574a560411e65dd4172e
S <s>
2014-06-25 18:41:19 -0700

    {"type":"TI", "runs":"1"}

--- Breakout.java
+++ Breakout.java
@@ -47,7 +47,7 @@ public class Breakout extends GraphicsProgram {
 /** Height of a brick */
 	private static final int BRICK_HEIGHT = 8;
 /** Bricks left on screen */
-	private int BRICKS_LEFT = 0;
+	private int BRICKS_LEFT = 100;
 
 /** Radius of the ball in pixels */
 	private static final int BALL_RADIUS = 10;
