641ee2881ea32ad0ac75da5e2b0c536c06a2e942
S <s>
2014-06-25 21:34:51 -0700

    {"type":"TI", "runs":"0"}

--- Breakout.java
+++ Breakout.java
@@ -155,6 +155,7 @@ public class Breakout extends GraphicsProgram {
 	
 	private void updateBall() {
 		Ball.move(vx, vy);
+		GObject BallCollider = ballCollider();
 		while(BRICKS_LEFT > 0) {
 			if(Ball.getX() <= 0) {
 				reflectOffSides();
