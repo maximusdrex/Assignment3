b54125d2596cb79bb6e7bf5224478d40902078b4
S <s>
2014-06-25 18:51:34 -0700

    {"type":"FR", "runs":"1"}

--- GCircle.java
+++
@@ -1,7 +0,0 @@
-import acm.graphics.*;
-/** Circle Class*/
-public class GCircle extends GOval {
-	public GCircle(double x, double y, double radius) {
-		this.setSize(2 * radius, 2 * radius);
-	}
-}
